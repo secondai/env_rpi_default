62b6908b9e299ecb7bc9d3194b4c729ccedb1036
Nicholas Reed <nicholas.a.reed@gmail.com>
2018-06-14 10:52:13 -0700

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 require('babel-register');
 require('dotenv').config();
-require('./init');
+// require('./init');
+console.log('STARTED!');
+setInterval(function(){
+	console.log('printing...');
+},5000);
