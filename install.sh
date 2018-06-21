fa0a6a13c1bc81001b7b9af385d69cdc0888768a
Nicholas Reed <nicholas.a.reed@gmail.com>
2018-06-20 19:23:07 -0700

    install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+{ # this ensures the entire script is downloaded #
+
+cd ~
+git clone https://github.com/secondai/env_rpi_default.git
+cd env_rpi_default
+docker-compose up
+
+} # this ensures the entire script is downloaded #
